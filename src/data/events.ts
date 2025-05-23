27e0ed5dd54b12474b01d74547b02af081ff4cd2
Kanishk Pachauri <itskanishkp.py@gmail.com>
2025-05-24 01:36:27 +0530

    feat: add info for march meetup

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,12 +1,12 @@
 export const upcomingEvents = [
     {
       id: 1,
-      title: "Pyworldwide monthly meetup (March Edition)",
-      date: "2025-03-08",
-      time: "6:00 PM PST",
+      title: "Pyworldwide monthly meetup (June Edition)",
+      date: "2025-06- 21",
+      time: "6:00 PM IST",
       location: "Virtual",
       description: "Dive into insightful talks by Python experts as they share their deep knowledge, experiences, and best practices to help you level up your skills!",
-      registrationLink: "https://example.com/register",
+      registrationLink: "",
       status: "upcoming"
     }
   ];
