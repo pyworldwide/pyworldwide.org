2a4c7e71f01b18e6928d759f5323853841f32280
Kanishk Pachauri <itskanishkp.py@gmail.com>
2025-05-24 20:24:46 +0530

    fix: open register now in a new tab.

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -81,14 +81,17 @@ export default function Events() {
                         </p>
                       </div>
                     </div>
-                    <Link
-                      href={event.registrationLink}
-                      className="bg-yellow-300 px-6 py-2 rounded-md border-2 border-black 
-                        shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] 
-                        hover:-translate-x-1 hover:-translate-y-1 transition-all duration-300 font-bold"
-                    >
-                      Register Now
-                    </Link>
+                    <Link href={event.registrationLink} passHref legacyBehavior>
+                        <a
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="bg-yellow-300 px-6 py-2 rounded-md border-2 border-black 
+                            shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] 
+                            hover:-translate-x-1 hover:-translate-y-1 transition-all duration-300 font-bold"
+                        >
+                          Register Now
+                        </a>
+                      </Link>
                   </div>
                   <p className="mt-4 text-gray-700">{event.description}</p>
                 </div>
